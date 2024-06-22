4b289841756128cd08b55e4988a38b1898d9d109
ignferry <ignasius.ferry01@gmail.com>
2024-06-22 19:56:33 +0700

    refactor: move some apply functionalities to utils

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,15 +1,11 @@
 import { initCommand } from "./init.js";
 import { applyCommand } from "./apply.js";
-import { executeTestCommand } from "./execute-test.js";
 import { deleteCommand } from "./delete.js";
-import { runCommand } from "./run.js";
 import { proxyResultCommand } from "./proxy-result.js";
 
 export function loadCommands(program) {
     program.addCommand(initCommand);
     program.addCommand(applyCommand);
-    program.addCommand(executeTestCommand);
     program.addCommand(proxyResultCommand);
     program.addCommand(deleteCommand);
-    program.addCommand(runCommand);
 }
